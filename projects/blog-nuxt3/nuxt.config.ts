f202465fcdb25df5312595a48075d4d8e452f041
yanhao <yanhoor@163.com>
2022-10-30 19:15:55 +0800

    feat: [blog-nuxt3] 添加 element-plus

--- projects/blog-nuxt3/nuxt.config.ts
+++ projects/blog-nuxt3/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
+
+const lifecycle = process.env.npm_lifecycle_event
 export default defineNuxtConfig({
+  build: {
+    transpile: lifecycle === "build" ? ["element-plus"] : [],
+  },
   css: ["@/assets/styles/global.scss"],
   vite: {
     css: {
@@ -8,6 +15,14 @@ export default defineNuxtConfig({
           additionalData: '@use "@/assets/styles/var.scss" as *;'
         }
       }
-    }
-  }
+    },
+    plugins: [
+      Components({
+        dts: true,
+        resolvers: [IconsResolver({})],
+      }),
+    ],
+  },
+  // auto import components
+  components: true,
 })
