ed699230fd912d5cc0e00de552a14f83a1520aed
yanhao <yanhoor@163.com>
2022-10-30 19:03:03 +0800

    feat: [blog-nuxt3] 全局变量/样式引入

--- projects/blog-nuxt3/nuxt.config.ts
+++ projects/blog-nuxt3/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  css: ["@/assets/styles/global.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/var.scss" as *;'
+        }
+      }
+    }
+  }
 })
