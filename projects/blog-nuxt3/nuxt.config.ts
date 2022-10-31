33ebcf0d1ab5a7b7541092e91e334df2c3143034
yanhao <yanhoor@163.com>
2022-10-31 10:59:16 +0800

    feat: [blog-nuxt3] 全局环境变量

--- projects/blog-nuxt3/nuxt.config.ts
+++ projects/blog-nuxt3/nuxt.config.ts
@@ -2,10 +2,19 @@
 import IconsResolver from "unplugin-icons/resolver";
 import Components from "unplugin-vue-components/vite";
 
+console.log('===============', process.env.NODE_ENV, process.env.BASE_API)
 const lifecycle = process.env.npm_lifecycle_event
 export default defineNuxtConfig({
   build: {
     transpile: lifecycle === "build" ? ["element-plus"] : [],
+  },
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123', // .env 文件里 NUXT_API_SECRET 的值(即NUXT_ 开头的值)
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: process.env.NUXT_API_BASE // 这个好像不会自动获取 NUXT_ 开头的值
+    }
   },
   css: ["@/assets/styles/global.scss"],
   vite: {
