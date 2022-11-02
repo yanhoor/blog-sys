4999b85c91d61d0dd61ce6aeb38c5e74d06e6007
yanhao <yanhoor@163.com>
2022-11-03 01:23:16 +0800

    fix: [blog-nuxt3] 黑暗模式下刷新页面后失效

--- projects/blog-nuxt3/app.vue
+++ projects/blog-nuxt3/app.vue
@@ -1,21 +1,28 @@
 <template>
   <NConfigProvider :theme="darkMode ? darkTheme : null">
-    <NuxtLayout>
-      <NuxtPage/>
-    </NuxtLayout>
+    <NuxtPage/>
   </NConfigProvider>
 </template>
 
 <script setup>
-import { darkTheme } from 'naive-ui'
-import { NConfigProvider } from 'naive-ui'
-import { useColorMode } from '@vueuse/core'
+import { NConfigProvider, darkTheme } from 'naive-ui'
 
 // const colorModel = useColorMode()
 const darkMode = useDarkMode()
 onMounted(() => {
-  console.log('=============app=======', darkMode.value, localStorage.getItem('vueuse-color-scheme'))
-  const s = localStorage.getItem('vueuse-color-scheme')
-  darkMode.value = s === 'dark'
+  // 还要加个setTimeout 主题才会换??
+  setTimeout(() => {
+    console.log('=======app=======', darkMode.value, localStorage.getItem('vueuse-color-scheme'))
+    const s = localStorage.getItem('vueuse-color-scheme')
+    darkMode.value = s === 'dark'
+    console.log('=============app2222=======', darkMode.value, process.client)
+
+    const userRandomKey = localStorage.getItem('random-key')
+    if(!userRandomKey){
+      const t = new Date().getTime()
+      const r = Math.floor(Math.random() * 9999 + 10000)
+      localStorage.setItem('random-key', t + '' + r)
+    }
+  }, 300)
 })
 </script>
