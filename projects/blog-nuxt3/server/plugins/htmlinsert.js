da423c4c5cf4dfd4f8f6c42f08fd2fd192bcc909
yanhao <yanhoor@163.com>
2022-11-11 14:11:54 +0800

    feat: [blog-nuxt3] 新增加载外部 script

---
+++ projects/blog-nuxt3/server/plugins/htmlinsert.js
@@ -0,0 +1,12 @@
+// https://v3.nuxtjs.org/guide/going-further/hooks
+// 插入额外资源
+
+export default defineNitroPlugin((nitroApp) => {
+  // nitroApp.hooks.hook('render:html', (html, { event }) => {
+  //   html.head.push('<script src="./prism.js"></script>');
+  // })
+  //
+  // nitroApp.hooks.hook('render:response', (response, { event }) => {
+  //   console.log('render:response', response)
+  // })
+})
