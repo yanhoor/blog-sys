c1f680b16bab4eedc27f79dfc1e29031c9ea3f21
yanhao <yanhoor@163.com>
2022-12-07 23:17:25 +0800

    feat: [blog-node] 新增通知接口

--- projects/blog-node/src/controller/notificationController.js
+++ projects/blog-node/src/controller/notificationController.js
@@ -43,6 +43,55 @@ class NotificationController extends BaseController{
       this.errorLogger.error('notification.list--------->', e)
     }
   }
+
+  info = async (ctx, next) => {
+    const { id } = ctx.request.body
+
+    try {
+      const result = await prisma.notification.findUnique({
+        where: {
+          id
+        },
+        select: {
+          id: true,
+          createdAt: true,
+          content: true,
+          isRead: true,
+          createById: true,
+          createBy: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true
+            }
+          }
+        }
+      })
+      return ctx.body = {
+        success: true,
+        result
+      }
+    }catch (e) {
+      this.errorLogger.error('notification.info--------->', e)
+    }
+  }
+
+  read = async (ctx, next) => {
+    const { id } = ctx.request.body
+    try {
+      await prisma.notification.update({
+        where: { id: Number(id) },
+        data: {
+          isRead: 1
+        }
+      })
+      return ctx.body = {
+        success: true
+      }
+    }catch (e) {
+      this.errorLogger.error('notification.read--------->', e)
+    }
+  }
 }
 
 module.exports = new NotificationController()
--- projects/blog-node/src/routes/visit/notification.js
+++ projects/blog-node/src/routes/visit/notification.js
@@ -4,5 +4,7 @@ const controller = require('../../controller/notificationController')
 router.prefix('/notification')
 
 router.post('/list', controller.list)
+router.post('/info', controller.info)
+router.post('/read', controller.read)
 
 module.exports = router
