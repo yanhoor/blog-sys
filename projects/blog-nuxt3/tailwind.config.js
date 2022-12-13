86e335bb6943a27ebbe9b3a2cba8c9f44feb2f69
yanhoor <yanhoor@163.com>
2022-12-13 16:38:20 +0800

    feat: [blog-nuxt3] Tailwindcss 配置

--- projects/blog-nuxt3/tailwind.config.js
+++ projects/blog-nuxt3/tailwind.config.js
@@ -1,6 +1,28 @@
 module.exports = {
   corePlugins: {
-
     preflight: false, // 禁止预设样式
+  },
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        page: {
+          light: '#e9ecef',
+          dark: '#1c1b22'
+        },
+        block: {
+          light: '#fff',
+          dark: '#303030'
+        },
+        content: {
+          light: '#F7F8FAB2',
+          dark: '#343434'
+        },
+        border: {
+          light: '#e4e7ed',
+          dark: '#282828'
+        },
+      }
+    }
   }
 }
