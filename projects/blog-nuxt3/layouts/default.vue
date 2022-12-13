e7c104025ab533609c6c25859523450d8cf11327
yanhoor <yanhoor@163.com>
2022-12-13 16:38:30 +0800

    feat: [blog-nuxt3] 使用 Tailwindcss 重写样式

--- projects/blog-nuxt3/layouts/default.vue
+++ projects/blog-nuxt3/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <n-layout class="default-layout">
-    <n-layout-header class="header">
-      <div class="header-left">
+  <n-layout class="default-layout bg-page-light dark:bg-page-dark">
+    <n-layout-header class="sticky h-[60px] z-50 left-0 right-0 top-0 flex py-0 px-[20px] items-center justify-between shadow bg-block-light dark:bg-block-dark">
+      <div class="flex">
         <n-button circle type="primary" @click="navigateTo('/')">
           <template #icon>
             <n-icon-wrapper :size="36" :border-radius="36">
@@ -11,7 +11,7 @@
           </template>
         </n-button>
       </div>
-      <n-space class="header-right">
+      <n-space class="items-center">
         <n-input placeholder="搜索关键字" v-model="searchWord">
           <template #prefix>
             <n-icon :component="Search12Regular" />
@@ -20,7 +20,7 @@
 
         <LayoutUser/>
 
-        <n-switch class="dark-switch" v-model:value="colorModel" @update:value="handleChange" size="large" checked-value="dark" unchecked-value="light">
+        <n-switch class="ml-[20px]" v-model:value="colorModel" @update:value="handleChange" size="large" checked-value="dark" unchecked-value="light">
           <template #checked-icon>
             <n-icon :component="WeatherMoon16Regular" />
           </template>
@@ -30,19 +30,19 @@
         </n-switch>
       </n-space>
     </n-layout-header>
-    <n-layout-content class="main-content">
+    <n-layout-content class="my-[20px] mx-auto overflow-visible w-[960px] bg-page-light dark:bg-page-dark lg:w-auto xl:w-[1140px]">
       <!--<div class="main-left">
         <slot name="left"></slot>
       </div>-->
-      <div class="main-right">
-        <div class="main-content-container">
+      <div class="flex items-start">
+        <div class="flex-1 max-w-full">
           <slot></slot>
         </div>
         <!--<div class="main-right-aside">
           <slot name="right">dwdwed</slot>
         </div>-->
       </div>
-      <div class="main-bottom">
+      <div class="mt-[20px]">
         <n-card>bottom</n-card>
       </div>
     </n-layout-content>
@@ -84,86 +84,8 @@ function handleChange(val: string) {
 <style lang="scss" scoped>
 .n-layout.default-layout{
   overflow: initial;
-  background-color: var(--page-background-color);
   :deep(.n-layout-scroll-container){
     overflow-x: initial;
   }
 }
-.header{
-  position: sticky;
-  height: var(--header-height);
-  z-index: 999;
-  left: 0;
-  right: 0;
-  top: 0;
-  display: flex;
-  padding: 0 20px;
-  justify-content: space-between;
-  align-items: center;
-  box-shadow: 0 1px 1px var(--n-border-color);
-  background-color: var(--block-background-color);
-  .header-left{
-    display: flex;
-  }
-  .header-right{
-    align-items: center;
-    .dark-switch{
-      margin-left: 12px;
-    }
-  }
-}
-
-.main-content{
-  margin: 20px auto;
-  overflow: visible;
-  width: 960px;
-  background-color: var(--page-background-color);
-  .main-left{
-    position: sticky;
-    top: calc(var(--header-height) + 60px);
-    width: 48px;
-    float: left;
-    margin-left: -60px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .main-right{
-    display: flex;
-    align-items: flex-start;
-    .main-content-container{
-      flex: 1;
-      max-width: 100%;
-    }
-    .main-right-aside{
-      width: 300px;
-      position: sticky;
-      top: calc(var(--header-height) + 60px);
-    }
-  }
-  .main-bottom{
-    margin-top: 20px;
-  }
-}
-
-@media (max-width: 1200px) {
-  .main-content{
-    width: auto;
-    margin-left: 20px;
-    margin-right: 20px;
-    .main-left{
-      display: none;
-    }
-    .main-right-aside{
-      display: none;
-    }
-  }
-}
-
-@media (min-width: 1200px) {
-  .main-content{
-    width: 1140px;
-  }
-}
-
 </style>
