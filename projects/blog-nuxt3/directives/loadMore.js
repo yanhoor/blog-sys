195ba6adec06fadd18f3f966331c879759dc288e
yanhoor <yanhoor@163.com>
2023-01-24 18:02:24 +0800

    feat: [blog-nuxt3] loadMore支持定义滚动元素和父元素

--- projects/blog-nuxt3/directives/loadMore.js
+++ projects/blog-nuxt3/directives/loadMore.js
@@ -1,14 +1,32 @@
 let callback = null
+let scrollEl = null
 function handleScroll(el, binding, vnode, prevVnode) {
+  let handler, parentElSelector, scrollElSelector
+  if(typeof binding.value === 'function') {
+    handler = binding.value
+  }else if(binding.value && typeof binding.value == 'object'){
+    ({ handler, parentElSelector, scrollElSelector } = binding.value)
+  }
   let timer
+  if(process.client){
+    scrollEl = scrollElSelector ? document.querySelector(scrollElSelector) : window
+  }
   callback = () => {
-    if(process.client){
+    if(process.client && typeof handler === 'function'){
       clearTimeout(timer)
       timer = setTimeout(() => {
         const info = el.getBoundingClientRect()
-        // console.log('----------handleScroll-----------', info.bottom, document.documentElement.clientHeight, typeof binding.value)
-        if(typeof binding.value === 'function' && info.bottom <= document.documentElement.clientHeight){
-          const finish = binding.value() // 绑定的函数返回 true 表示已经全部加载完，移除监听
+        // console.log('----------handleScroll-----------', parentElSelector, info.bottom - document.documentElement.clientHeight)
+        if(parentElSelector){
+          // 参数为父元素的id
+          const parent = document.querySelector(parentElSelector)
+          const parentInfo = parent.getBoundingClientRect()
+          if(info.bottom - parentInfo.bottom <= 50 ){
+            const finish = handler() // 绑定的函数返回 true 表示已经全部加载完，移除监听
+            if(finish) document.removeEventListener('scroll', callback)
+          }
+        }else if(info.bottom - document.documentElement.clientHeight <= 50){
+          const finish = handler() // 绑定的函数返回 true 表示已经全部加载完，移除监听
           if(finish) document.removeEventListener('scroll', callback)
         }
       }, 200)
@@ -19,10 +37,10 @@ function handleScroll(el, binding, vnode, prevVnode) {
 const loadMore = {
   mounted(el, binding, vnode, prevVnode) {
     handleScroll(el, binding, vnode, prevVnode)
-    document.addEventListener('scroll', callback)
+    scrollEl.addEventListener('scroll', callback)
   },
   unmounted(el, binding, vnode, prevVnode) {
-    document.removeEventListener('scroll', callback)
+    scrollEl.removeEventListener('scroll', callback)
   }
 }
 
