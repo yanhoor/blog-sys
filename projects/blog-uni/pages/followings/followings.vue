4dbcac0e6150891c90cf55479689662c0321feb1
yanhoor <yanhoor@163.com>
2023-02-20 16:50:08 +0800

    feat: [blog-uni] 新增关注/粉丝列表页面

---
+++ projects/blog-uni/pages/followings/followings.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+		<uni-card margin="5px" spacing="0">
+			<Userlist :pageUrl="pageUrl" :url="urls.user_friends" :searchParams="{ uid: userId, relateType: 1 }"></Userlist>
+		</uni-card>
+	</view>
+</template>
+
+<script>
+	import Userlist from '@/components/user/user-list.vue'
+	import scrollMixin from '@/mixins/scrollMixin.js'
+	import {
+		useScrollStatusStore
+	} from '@/stores/scrollStatus.js'
+	import Http, {
+		urls
+	} from '@/http'
+
+	export default {
+		mixins: [scrollMixin],
+		components: {
+			Userlist
+		},
+		data() {
+			return {
+				pageUrl: '/pages/followings',
+				userId: '',
+				urls
+			}
+		},
+		onLoad(params) {
+			this.userId = params.userId
+			uni.startPullDownRefresh()
+		},
+		onPullDownRefresh() {
+			const s = useScrollStatusStore()
+			s.setPullDownRefresh(this.pageUrl)
+		},
+	}
+</script>
+
+<style lang="scss">
+
+</style>
