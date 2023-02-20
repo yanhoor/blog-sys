f55ac4368ba72b0e48e7581090e2ef39b7e725e1
yanhoor <yanhoor@163.com>
2023-02-20 16:28:51 +0800

    feat: [blog-uni] 新增搜索

---
+++ projects/blog-uni/pages/search-result/search-result.vue
@@ -0,0 +1,139 @@
+<template>
+	<view class="search-result-page">
+		<view class="search-section">
+			<uni-card margin="5px" padding="0" spacing="0" isFull>
+				<view class="search-container">
+					<uni-search-bar class="search-bar" v-model="searchParams.keyword" @confirm="handleSearch" bgColor="#f0f0f0"
+						@cancel="handleBack"></uni-search-bar>
+					<!-- <uni-icons type="tune" size="24" @click="showFilter = !showFilter" color="#d7d7d7"></uni-icons> -->
+				</view>
+				<view class="filter-container" v-if="showFilter">
+					<uni-data-select v-model="searchParams.sort" :clear="false" :localdata="sortTypeList" @change="handleSearch">
+					</uni-data-select>
+					<uni-data-select v-model="searchParams.time" :clear="false" :localdata="filterTimeList" @change="handleSearch">
+					</uni-data-select>
+				</view>
+			</uni-card>
+		</view>
+		<PostList :pageUrl="pageUrl" :searchParams="searchParams" :showSkeleton="false"></PostList>
+	</view>
+</template>
+
+<script>
+	import PostList from '@/components/post/post-list.vue'
+	import scrollMixin from '@/mixins/scrollMixin.js'
+	import {
+		useScrollStatusStore
+	} from '@/stores/scrollStatus.js'
+
+	export default {
+		mixins: [scrollMixin],
+		components: {
+			PostList
+		},
+		data() {
+			return {
+				pageUrl: '/pages/search',
+				showFilter: false,
+				searchParams: {
+					keyword: '',
+					sort: 1,
+					time: 0
+				},
+				historyList: [],
+				sortTypeList: [{
+						value: 1,
+						text: "综合排序"
+					},
+					{
+						value: 2,
+						text: "最新优先"
+					},
+					{
+						value: 3,
+						text: "最热优先"
+					},
+				],
+				filterTimeList: [{
+						value: 0,
+						text: "时间不限"
+					},
+					{
+						value: 1,
+						text: "最近一天"
+					},
+					{
+						value: 2,
+						text: "最近一周"
+					},
+					{
+						value: 3,
+						text: "最近三个月"
+					},
+				]
+			}
+		},
+		created() {
+			this.historyList = JSON.parse(uni.getStorageSync('search-history')) || []
+		},
+		onLoad(params) {
+			this.searchParams.keyword = params.keyword
+			uni.startPullDownRefresh()
+		},
+		onPullDownRefresh() {
+			const s = useScrollStatusStore()
+			if (this.searchParams.keyword) {
+				s.setPullDownRefresh(this.pageUrl)
+			} else {
+				uni.stopPullDownRefresh()
+			}
+		},
+		methods: {
+			handleSearch() {
+				if (!this.searchParams.keyword) {
+					uni.showToast({
+						icon: 'error',
+						title: '请输入内容'
+					})
+					return
+				}
+
+				if (!this.historyList.includes(this.searchParams.keyword)) {
+					this.historyList.push(this.searchParams.keyword)
+					uni.setStorageSync('search-history', JSON.stringify(this.historyList))
+				}
+
+				uni.startPullDownRefresh()
+			},
+			handleBack() {
+				uni.navigateBack()
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.search-section {
+		position: sticky;
+		top: 0;
+		z-index: 1;
+	}
+	
+	.search-container{
+		display: flex;
+		align-items: center;
+		gap: 12rpx;
+		// padding-right: 10px;
+		.search-bar{
+			flex: 1;
+		}
+	}
+
+	.filter-container {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		gap: 24rpx;
+		z-index: 1;
+	}
+</style>
---
+++ projects/blog-uni/pages/search/search.vue
@@ -0,0 +1,131 @@
+<template>
+	<view class="search-page">
+		<view class="input-container">
+			<uni-easyinput v-model="keyword" placeholder="输入要搜索的内容" clearable :trim="true" confirmType="search"
+				@confirm="handleSearch" />
+			<button type="primary" size="mini" @click="handleSearch">搜索</button>
+		</view>
+		<view class="section-block" v-if="historyList.length">
+			<view class="secction-title-container">
+				<view class="section-title">
+					搜索历史
+				</view>
+				<view class="section-action" @click="handleClearHistory">
+					清除
+				</view>
+			</view>
+			<uni-card margin="5px" spacing="0">
+				<view class="history-container" v-for="(item, index) in historyList" :key="item">
+					<view class="item" @click="handleClickHistoryItem(item)">
+						{{ item }}
+					</view>
+					<uni-icons type="closeempty" color="#c7c7c7" @click.stop="handleDeleteHistoryItem(index)">
+					</uni-icons>
+				</view>
+			</uni-card>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				keyword: '',
+				historyList: []
+			}
+		},
+		onShow() {
+			this.historyList = JSON.parse(uni.getStorageSync('search-history')) || []
+		},
+		methods: {
+			handleIconClick(type) {
+				if (type === 'suffix') {
+					this.handleSearch()
+				}
+			},
+			handleClickHistoryItem(item) {
+				this.keyword = item
+				this.handleSearch()
+			},
+			handleSearch() {
+				if (!this.keyword) {
+					uni.showToast({
+						icon: 'error',
+						title: '请输入内容'
+					})
+					return
+				}
+
+				if (!this.historyList.includes(this.keyword)) {
+					this.historyList.push(this.keyword)
+					uni.setStorageSync('search-history', JSON.stringify(this.historyList))
+				}
+
+				uni.navigateTo({
+					url: '/pages/search-result/search-result?keyword=' + this.keyword
+				})
+			},
+			handleDeleteHistoryItem(index) {
+				this.historyList.splice(index, 1)
+				uni.setStorageSync('search-history', JSON.stringify(this.historyList))
+			},
+			handleClearHistory() {
+				this.historyList = []
+				uni.setStorageSync('search-history', JSON.stringify(this.historyList))
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.search-page {
+		display: flex;
+		flex-direction: column;
+		gap: 24rpx;
+	}
+
+	.section-block {
+		display: flex;
+		flex-direction: column;
+		gap: 10rpx;
+
+		.secction-title-container {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			color: $uni-secondary-color;
+			margin: 0 5px;
+
+			.section-title {
+				font-size: 14px;
+				font-weight: 600;
+			}
+
+			.section-action {
+				font-size: 14px;
+			}
+		}
+	}
+
+	.input-container {
+		display: flex;
+		align-items: center;
+		gap: 12rpx;
+		margin: 0 5px;
+	}
+
+	.history-container {
+		display: flex;
+		align-items: center;
+		padding: 20rpx 0;
+
+		&+.history-container {
+			border-top: 1px solid $uni-border-3;
+		}
+
+		.item {
+			flex: 1;
+		}
+	}
+</style>
