ae9508c1757c4422ee255133868d70562e57c625
yanhoor <yanhoor@163.com>
2023-02-20 20:34:52 +0800

    feat: [blog-uni] 配置调整

--- projects/blog-uni/App.vue
+++ projects/blog-uni/App.vue
@@ -19,19 +19,19 @@
 
 <style lang="scss">
 	/*每个页面公共css */
+	@import 'static/styles/iconfont.css';
 	@import '@/uni_modules/uni-scss/index.scss';
+	@import '@/static/styles/common.scss';
 
 	/* #ifndef APP-NVUE */
-	@import '@/static/customicons.css';
+	@import '@/static/styles/customicons.css';
 	// 设置整个项目的背景色
 	page {
 		background-color: #f5f5f5;
 	}
 
 	/* #endif */
-	.example-info {
-		font-size: 14px;
-		color: #333;
-		padding: 10px;
+	uni-icons{
+		line-height: 1;
 	}
 </style>
