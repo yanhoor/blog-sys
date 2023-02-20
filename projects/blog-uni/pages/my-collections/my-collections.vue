81d2dfd9a7284dc5020dd958fab2cc921d973f4f
yanhoor <yanhoor@163.com>
2023-02-20 17:03:14 +0800

    feat: [blog-uni] 新增我的收藏/我的点赞页面

---
+++ projects/blog-uni/pages/my-collections/my-collections.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<PostList :pageUrl="pageUrl" :url="urls.user_markBlogList" :searchParams="{ type: 2 }"></PostList>
+	</view>
+</template>
+
+<script>
+	import PostList from '@/components/post/post-list.vue'
+	import scrollMixin from '@/mixins/scrollMixin.js'
+	import {
+		useScrollStatusStore
+	} from '@/stores/scrollStatus.js'
+	import Http, {
+		urls
+	} from '@/http'
+
+	export default {
+		mixins: [scrollMixin],
+		components: {
+			PostList
+		},
+		data() {
+			return {
+				pageUrl: '/pages/my-collections',
+				urls
+			}
+		},
+		onLoad(params) {
+			uni.startPullDownRefresh()
+		},
+		onPullDownRefresh() {
+			const s = useScrollStatusStore()
+			s.setPullDownRefresh(this.pageUrl)
+		},
+	}
+</script>
+
+<style lang="scss">
+
+</style>
