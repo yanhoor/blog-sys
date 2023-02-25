b931abf6d6eace9bd2d6e87ee65cd04585df49f8
yanhoor <yanhoor@163.com>
2023-02-25 13:48:22 +0800

    feat: [blog-nuxt3] 展开内容支持多行

--- projects/blog-nuxt3/components/ExpandableContent.vue
+++ projects/blog-nuxt3/components/ExpandableContent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="whitespace-pre-wrap break-words transition-all">
     {{ isExpanded ? content : getPostSummary(content) }}
-    <n-button text type="primary" @click="handleExpand" v-if="content?.length > maxLength">{{ isExpanded ? '收起' : '展开' }}</n-button>
+    <n-button text type="primary" @click="handleExpand" v-if="showAction">{{ isExpanded ? '收起' : '展开' }}</n-button>
   </div>
 </template>
 
@@ -10,16 +10,30 @@ import { NButton } from "naive-ui"
 interface Props{
   content: string
   maxLength?: number
+  maxLine?: number
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  maxLength: 280
+  maxLength: 280,
+  maxLine: 3
 })
 const isExpanded = ref(false)
 const scrollTop = ref(0)
 
+const lineContentList = computed(() => {
+  return props.content.split('\n')
+})
+const showAction = computed(() => {
+  if(lineContentList.value.length > props.maxLine) return true
+  if(props.content?.length > props.maxLength) return true
+
+  return false
+})
+
 function getPostSummary(content: string) {
-  if(content && content.length > props.maxLength){
+  if(lineContentList.value.length > props.maxLine){
+    return lineContentList.value.slice(0, props.maxLine).join('\n') + '\n'
+  }else if(content && content.length > props.maxLength){
     return content.slice(0, props.maxLength) + '...'
   }else{
     return content
