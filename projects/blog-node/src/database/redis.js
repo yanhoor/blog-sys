a7b40a1eb3b24ad77af8a4e88517250af32e6751
yanhoor <yanhoor@163.com>
2023-03-29 13:16:28 +0800

    feat: [blog-node] redis

--- projects/blog-node/src/database/redis.js
+++ projects/blog-node/src/database/redis.js
@@ -7,9 +7,14 @@ class RedisClient{
     // this.redisClient = createClient(config.redis)
     const url = `redis://${config.redis.username}:${config.redis.password}@${config.redis.host}:${config.redis.port}`
     console.log('======redis url=======', url)
+    // this.redisClient = createClient({
+    //   // 格式: redis[s]://[[username][:password]@][host][:port][/db-number]
+    //   url
+    // })
     this.redisClient = createClient({
-      // 格式: redis[s]://[[username][:password]@][host][:port][/db-number]
-      url
+      // username: config.redis.username, // 这个会报错 Redis Client Error [ErrorReply: ERR wrong number of arguments for 'auth' command]?
+      password: config.redis.password,
+      url: `redis://${config.redis.host}:${config.redis.port}`
     })
 
     this.redisClient.on('error', (err) => console.log('Redis Client Error', err))
