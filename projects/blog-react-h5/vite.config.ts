b2009a709363e7351c9e4325652ca69b25f65cf1
yanhoor <yanhoor@163.com>
2023-03-31 16:30:16 +0800

    feat: [blog-react-h5] 新增运行配置/环境变量

--- projects/blog-react-h5/vite.config.ts
+++ projects/blog-react-h5/vite.config.ts
@@ -1,16 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    port: 7878
-  },
+export default defineConfig(({ command, mode }) => {
+  // 参考 https://cn.vitejs.dev/config/#using-environment-variables-in-config
+  // Vite 默认是不加载 .env 文件的，因为这些文件需要在执行完 Vite 配置后才能确定加载哪一个
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    server: {
+      port: 7878,
+      proxy: {
+        '/api': {
+          // target: 'http://localhost:8000',
+          target: env.VITE_API_BASE,
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+  }
 })
