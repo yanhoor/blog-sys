2fe0384549824d67dd20a1613faf891b5edaa908
yanhoor <yanhoor@163.com>
2023-05-26 21:52:44 +0800

    feat: [blog-manage] 新增图片组件

---
+++ projects/blog-manage/src/components/media/media_image_item.vue
@@ -0,0 +1,23 @@
+<template>
+  <img :src="imageUrl" />
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref, computed } from 'vue'
+import $http, { urls, IMG_HOST } from '@/http'
+
+interface Props {
+  url: string
+  quality: string | number
+}
+
+const props = defineProps<Props>()
+
+const imageUrl = computed(() => {
+  let result = IMG_HOST + props.url
+
+  if (props.quality) result += '?x-oss-process=image/resize,p_' + props.quality
+
+  return result
+})
+</script>
