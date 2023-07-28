046e25aeb1473b5597031686a7168b15c130b9c0
yanhoor <yanhoor@163.com>
2023-07-28 17:10:45 +0800

    feat: [blog-h5] 添加点击跳到用户主页

--- projects/blog-h5/src/components/user/user-avatar.tsx
+++ projects/blog-h5/src/components/user/user-avatar.tsx
@@ -6,14 +6,17 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom'
 interface Props {
   user: User
   size?: number | string
+  enable?: boolean
 }
 
-export default function UserAvatar({ user, size = 38 }: Props) {
+export default function UserAvatar({ user, size = 38, enable = true }: Props) {
   const ErrorIcon = <UserO />
   const navigate = useNavigate()
   const location = useLocation()
 
   function toUserPage(e: any) {
+    if (!enable) return
+
     e.stopPropagation()
     if (location.pathname === '/user/' + user.id) return
 
--- projects/blog-h5/src/components/user/user-name.tsx
+++ projects/blog-h5/src/components/user/user-name.tsx
@@ -1,16 +1,35 @@
 import { User } from 'sys-types'
+import { useNavigate } from 'react-router-dom'
 
 interface Props {
   user: User
   showAt?: boolean
+  enable?: boolean
   className?: string
 }
-export default function UserName({ user, showAt = false, className }: Props) {
+export default function UserName({
+  user,
+  showAt = false,
+  enable = true,
+  className
+}: Props) {
+  const navigate = useNavigate()
+
+  function toUserPage(e: any) {
+    if (!enable) return
+
+    e.stopPropagation()
+    if (location.pathname === '/user/' + user.id) return
+
+    navigate('/user/' + user.id)
+  }
+
   return (
     <span
       className={`user-name whitespace-pre-wrap break-words text-primary ${
         className || ''
       }`}
+      onClick={toUserPage}
     >
       {(showAt ? '@' : '') + user.name}
     </span>
