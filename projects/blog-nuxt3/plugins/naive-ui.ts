31e2544ec27fe84db9b8593b321131691e039ee6
yanhoor <yanhoor@163.com>
2023-09-15 02:15:23 +0800

    fixed: [blog-nuxt3] 升级 Nuxt 3.7.0 后刷新页面样式错乱

--- projects/blog-nuxt3/plugins/naive-ui.ts
+++ projects/blog-nuxt3/plugins/naive-ui.ts
@@ -1,31 +1,28 @@
 import { setup } from '@css-render/vue3-ssr'
-import { defineNuxtPlugin } from '#app'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  if (process.server) {
-    const { collect } = setup(nuxtApp.vueApp)
-    const originalRenderMeta = nuxtApp.ssrContext?.renderMeta
-    nuxtApp.ssrContext = nuxtApp.ssrContext || {}
-    nuxtApp.ssrContext.renderMeta = () => {
-      if (!originalRenderMeta) {
-        return {
-          headTags: collect()
-        }
-      }
-      const originalMeta = originalRenderMeta()
-      if ('then' in originalMeta) {
-        return originalMeta.then((resolvedOriginalMeta) => {
-          return {
-            ...resolvedOriginalMeta,
-            headTags: resolvedOriginalMeta['headTags'] + collect()
-          }
-        })
-      } else {
-        return {
-          ...originalMeta,
-          headTags: originalMeta['headTags'] + collect()
-        }
-      }
+export default defineNuxtPlugin({
+  name: 'naive-ui',
+  enforce: 'pre',
+  setup(nuxtApp) {
+    if (process.server) {
+      const { collect } = setup(nuxtApp.vueApp)
+      nuxtApp.ssrContext?.head.hooks.hook('tags:resolve', (ctx) => {
+        //  insert Style after meta
+        const lastMetaIndex = ctx.tags.map((x) => x.tag).lastIndexOf('meta')
+        const styleTags = collect()
+          .split('</style>')
+          .filter(Boolean)
+          .map((x) => {
+            const id = x.match(/cssr-id="(.+?)"/)?.[1]
+            const style = (x.match(/>(.*)/s)?.[1] || '').trim()
+            return {
+              tag: 'style',
+              props: { 'cssr-id': id },
+              innerHTML: style
+            }
+          })
+        ctx.tags.splice(lastMetaIndex + 1, 0, ...styleTags)
+      })
     }
   }
 })
