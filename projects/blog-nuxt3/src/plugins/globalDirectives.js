528999f96186ef17bae2eb006a80e3d2423d3183
yanhao <yanhao@smallrig.com>
2024-09-13 14:52:55 +0800

    fix: [blog-nuxt3]  Cannot read properties of undefined (reading 'body')

--- projects/blog-nuxt3/src/plugins/globalDirectives.js
+++ projects/blog-nuxt3/src/plugins/globalDirectives.js
@@ -1,5 +1,4 @@
 import directives from '@/directives'
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
 // 注册全局指令，https://nuxt.com/docs/guide/directory-structure/plugins，参考底部
 export default defineNuxtPlugin((nuxtApp) => {
@@ -7,5 +6,4 @@ export default defineNuxtPlugin((nuxtApp) => {
     // console.log('====defineNuxtPlugin directives=====', import.meta.client, k, directives[k])
     nuxtApp.vueApp.directive(k, directives[k])
   }
-  nuxtApp.vueApp.directive(autoAnimatePlugin)
 })
