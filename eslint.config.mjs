49d6938885a836fa382e4a2548aac097476eebbd
yanhao <yanhao@smallrig.com>
2024-11-13 16:30:37 +0800

    feat: eslint 配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,45 +10,61 @@ import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 /** @type {import('eslint').Linter.Config[]} */
 export default createConfigForNuxt({
   // options here
-}).prepend([
-  { files: ['**/*.{js,mjs,cjs,ts,vue,jsx,tsx}'] },
-  {
-    languageOptions: {
-      globals: globals.browser
-    }
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  ...pluginVue.configs['flat/essential'],
-  {
-    files: ['**/*.vue'],
-    languageOptions: { parserOptions: { parser: tseslint.parser } }
-  },
-  eslintConfigPrettier,
-  eslintPluginPrettierRecommended,
-  {
-    ignores: [
-      '**/logs/',
-      '**/*.log',
-      '**/*.log*',
-      '**/node_modules/',
-      '**/.vscode/',
-      '**/.husky/',
-      '**/.nuxt/',
-      '**/.idea/',
-      '**/.output/',
-      '**/.local/',
-      '**/dist/',
-      '**/public/',
-      '**/*.sh',
-      '**/*.md',
-      '**/*.woff',
-      '**/*.ttf'
-    ],
+})
+  .prepend(
+    // ...Prepend some flat configs in front
+    [
+      { files: ['**/*.{js,mjs,cjs,ts,vue,jsx,tsx}'] },
+      {
+        languageOptions: {
+          globals: globals.browser
+        }
+      },
+      pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      ...pluginVue.configs['flat/essential'],
+      {
+        files: ['**/*.vue'],
+        languageOptions: { parserOptions: { parser: tseslint.parser } }
+      },
+      eslintConfigPrettier,
+      eslintPluginPrettierRecommended,
+      {
+        ignores: [
+          '**/logs/',
+          '**/*.log',
+          '**/*.log*',
+          '**/node_modules/',
+          '**/.vscode/',
+          '**/.husky/',
+          '**/.nuxt/',
+          '**/.idea/',
+          '**/.output/',
+          '**/.local/',
+          '**/dist/',
+          '**/public/',
+          '**/*.sh',
+          '**/*.md',
+          '**/*.woff',
+          '**/*.ttf'
+        ],
+        rules: {
+          'no-unused-vars': 'warn'
+        }
+      }
+    ]
+  )
+  // Override some rules in a specific config, based on their name
+  .override('nuxt/typescript/rules', {
     rules: {
-      'vue/multi-word-component-names': 'off',
-      'no-unused-vars': 'warn'
+      // ...Override rules, for example:
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn'
     }
-  }
-])
+  })
+  .override('nuxt/vue/rules', {
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
+  })
