2e7cbd8990e573060f166af6f1981347f50732d6
yanhao <yanhao@smallrig.com>
2024-11-15 18:47:29 +0800

    feat: [blog-node] prisma migrate 初始化

---
+++ projects/blog-node/prisma/migrations/20241115091017_initial_migration/migration.sql
@@ -0,0 +1,357 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `lastActiveAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `lock` INTEGER NOT NULL DEFAULT 2,
+    `name` VARCHAR(255) NOT NULL,
+    `avatar` VARCHAR(255) NULL,
+    `profileCardBg` VARCHAR(255) NULL,
+    `introduce` VARCHAR(255) NULL,
+    `mobile` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `type` INTEGER NOT NULL DEFAULT 2,
+    `gender` INTEGER NOT NULL DEFAULT 0,
+    `birthday` DATETIME(3) NULL,
+    `openid` VARCHAR(255) NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `User_name_key`(`name`),
+    UNIQUE INDEX `User_mobile_key`(`mobile`),
+    UNIQUE INDEX `User_openid_key`(`openid`),
+    INDEX `User_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `FollowGroup` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `createById` VARCHAR(191) NOT NULL,
+    `sort` INTEGER NOT NULL DEFAULT 0,
+    `name` VARCHAR(255) NOT NULL,
+    `system` INTEGER NOT NULL DEFAULT 2,
+    `deletedAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `FollowRelation` (
+    `userId` VARCHAR(191) NOT NULL,
+    `followById` VARCHAR(191) NOT NULL,
+    `assignedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`userId`, `followById`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LikeBlogRelation` (
+    `userId` VARCHAR(191) NOT NULL,
+    `blogId` VARCHAR(191) NOT NULL,
+    `assignedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`userId`, `blogId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UserCollectBlogs` (
+    `userId` VARCHAR(191) NOT NULL,
+    `blogId` VARCHAR(191) NOT NULL,
+    `assignedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`userId`, `blogId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ReadBlogsRelation` (
+    `userId` VARCHAR(191) NOT NULL,
+    `blogId` VARCHAR(191) NOT NULL,
+    `assignedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`userId`, `blogId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Media` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createById` VARCHAR(191) NOT NULL,
+    `title` TEXT NULL,
+    `fileId` VARCHAR(191) NOT NULL,
+    `coverId` VARCHAR(191) NULL,
+    `blogId` VARCHAR(191) NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    INDEX `Media_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `File` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createById` VARCHAR(191) NOT NULL,
+    `md5` TEXT NOT NULL,
+    `url` TEXT NOT NULL,
+    `type` ENUM('video', 'audio', 'image') NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    INDEX `File_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Blog` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createById` VARCHAR(191) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+    `auditById` VARCHAR(191) NULL,
+    `auditTip` VARCHAR(255) NULL,
+    `auditStatus` INTEGER NOT NULL DEFAULT 0,
+    `auditedAt` DATETIME(3) NULL,
+    `status` INTEGER NOT NULL DEFAULT 1,
+    `contentType` INTEGER NOT NULL DEFAULT 1,
+    `address` TEXT NULL,
+    `addressName` TEXT NULL,
+    `latitude` DOUBLE NULL,
+    `longitude` DOUBLE NULL,
+    `content` LONGTEXT NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+    `retweetOriginBlogId` VARCHAR(191) NULL,
+
+    INDEX `Blog_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Topic` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createById` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `Topic_content_key`(`content`),
+    INDEX `content`(`content`(20)),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `BlogTopicRelation` (
+    `blogId` VARCHAR(191) NOT NULL,
+    `topicId` VARCHAR(191) NOT NULL,
+    `assignedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`blogId`, `topicId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Notification` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createById` VARCHAR(191) NOT NULL,
+    `receiveUserId` VARCHAR(191) NULL,
+    `blogId` VARCHAR(191) NULL,
+    `commentId` VARCHAR(191) NULL,
+    `content` JSON NULL,
+    `isRead` INTEGER NULL DEFAULT 0,
+    `type` ENUM('collect_blog', 'like_blog', 'comment', 'comment_reply', 'system_audit') NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    INDEX `Notification_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createById` VARCHAR(191) NOT NULL,
+    `auditById` VARCHAR(191) NULL,
+    `auditTip` VARCHAR(255) NULL,
+    `auditStatus` INTEGER NOT NULL DEFAULT 0,
+    `auditedAt` DATETIME(3) NULL,
+    `status` INTEGER NOT NULL DEFAULT 1,
+    `replyToId` VARCHAR(191) NULL,
+    `blogId` VARCHAR(191) NOT NULL,
+    `topCommentId` VARCHAR(191) NULL,
+    `replyCommentId` VARCHAR(191) NULL,
+    `imageId` VARCHAR(191) NULL,
+    `content` TEXT NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    INDEX `Comment_createdAt_idx`(`createdAt`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UserLikeCommentRelation` (
+    `userId` VARCHAR(191) NOT NULL,
+    `commentId` VARCHAR(191) NOT NULL,
+    `assignedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`userId`, `commentId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UpdateVersion` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createById` VARCHAR(191) NOT NULL,
+    `platform` ENUM('android', 'ios', 'miniProgram') NOT NULL,
+    `publish` BOOLEAN NOT NULL DEFAULT false,
+    `forceUpdate` BOOLEAN NOT NULL DEFAULT false,
+    `url` TEXT NOT NULL,
+    `version` TEXT NOT NULL,
+    `changeLog` LONGTEXT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CustomContent` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createById` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_InFollowGroup` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_InFollowGroup_AB_unique`(`A`, `B`),
+    INDEX `_InFollowGroup_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_BlogRefernce` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_BlogRefernce_AB_unique`(`A`, `B`),
+    INDEX `_BlogRefernce_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `FollowGroup` ADD CONSTRAINT `FollowGroup_createById_fkey` FOREIGN KEY (`createById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `FollowRelation` ADD CONSTRAINT `FollowRelation_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `FollowRelation` ADD CONSTRAINT `FollowRelation_followById_fkey` FOREIGN KEY (`followById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LikeBlogRelation` ADD CONSTRAINT `LikeBlogRelation_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LikeBlogRelation` ADD CONSTRAINT `LikeBlogRelation_blogId_fkey` FOREIGN KEY (`blogId`) REFERENCES `Blog`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserCollectBlogs` ADD CONSTRAINT `UserCollectBlogs_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserCollectBlogs` ADD CONSTRAINT `UserCollectBlogs_blogId_fkey` FOREIGN KEY (`blogId`) REFERENCES `Blog`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ReadBlogsRelation` ADD CONSTRAINT `ReadBlogsRelation_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ReadBlogsRelation` ADD CONSTRAINT `ReadBlogsRelation_blogId_fkey` FOREIGN KEY (`blogId`) REFERENCES `Blog`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Media` ADD CONSTRAINT `Media_createById_fkey` FOREIGN KEY (`createById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Media` ADD CONSTRAINT `Media_fileId_fkey` FOREIGN KEY (`fileId`) REFERENCES `File`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Media` ADD CONSTRAINT `Media_coverId_fkey` FOREIGN KEY (`coverId`) REFERENCES `File`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Media` ADD CONSTRAINT `Media_blogId_fkey` FOREIGN KEY (`blogId`) REFERENCES `Blog`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `File` ADD CONSTRAINT `File_createById_fkey` FOREIGN KEY (`createById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Blog` ADD CONSTRAINT `Blog_createById_fkey` FOREIGN KEY (`createById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Blog` ADD CONSTRAINT `Blog_auditById_fkey` FOREIGN KEY (`auditById`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Blog` ADD CONSTRAINT `Blog_retweetOriginBlogId_fkey` FOREIGN KEY (`retweetOriginBlogId`) REFERENCES `Blog`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Topic` ADD CONSTRAINT `Topic_createById_fkey` FOREIGN KEY (`createById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BlogTopicRelation` ADD CONSTRAINT `BlogTopicRelation_blogId_fkey` FOREIGN KEY (`blogId`) REFERENCES `Blog`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `BlogTopicRelation` ADD CONSTRAINT `BlogTopicRelation_topicId_fkey` FOREIGN KEY (`topicId`) REFERENCES `Topic`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Notification` ADD CONSTRAINT `Notification_createById_fkey` FOREIGN KEY (`createById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Notification` ADD CONSTRAINT `Notification_receiveUserId_fkey` FOREIGN KEY (`receiveUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Notification` ADD CONSTRAINT `Notification_blogId_fkey` FOREIGN KEY (`blogId`) REFERENCES `Blog`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Notification` ADD CONSTRAINT `Notification_commentId_fkey` FOREIGN KEY (`commentId`) REFERENCES `Comment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_createById_fkey` FOREIGN KEY (`createById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_auditById_fkey` FOREIGN KEY (`auditById`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_replyToId_fkey` FOREIGN KEY (`replyToId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_blogId_fkey` FOREIGN KEY (`blogId`) REFERENCES `Blog`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_topCommentId_fkey` FOREIGN KEY (`topCommentId`) REFERENCES `Comment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_replyCommentId_fkey` FOREIGN KEY (`replyCommentId`) REFERENCES `Comment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_imageId_fkey` FOREIGN KEY (`imageId`) REFERENCES `File`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserLikeCommentRelation` ADD CONSTRAINT `UserLikeCommentRelation_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserLikeCommentRelation` ADD CONSTRAINT `UserLikeCommentRelation_commentId_fkey` FOREIGN KEY (`commentId`) REFERENCES `Comment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UpdateVersion` ADD CONSTRAINT `UpdateVersion_createById_fkey` FOREIGN KEY (`createById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CustomContent` ADD CONSTRAINT `CustomContent_createById_fkey` FOREIGN KEY (`createById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_InFollowGroup` ADD CONSTRAINT `_InFollowGroup_A_fkey` FOREIGN KEY (`A`) REFERENCES `FollowGroup`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_InFollowGroup` ADD CONSTRAINT `_InFollowGroup_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_BlogRefernce` ADD CONSTRAINT `_BlogRefernce_A_fkey` FOREIGN KEY (`A`) REFERENCES `Blog`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_BlogRefernce` ADD CONSTRAINT `_BlogRefernce_B_fkey` FOREIGN KEY (`B`) REFERENCES `Blog`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
